7b8c08a14b118656222b903425b7673fe586516a
Anna Manukian <amanu001@odu.edu>
2019-04-28 21:54:40 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -74,6 +74,13 @@ def over?(board)
 end
 
 
-def winner()
-
+def winner(board)
+  win_combination = won?(board)
+  if win_combination[0] == "X"
+    return "X"
+  elsif win_combination[0] == "O"
+    return "O"
+  else
+    return nil
+  end
 end
