7c9700fb7fbe43672fcba08f69100658aa8253e6
Anna Manukian <amanu001@odu.edu>
2019-04-28 22:13:33 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -58,11 +58,12 @@ def full?(board)
 end
 
 def draw?(board)
+  if won?(board)
+    return false
+  end
   if !won?(board) && full?(board)
     return true
   elsif !won?(board) && !full?(board)
-    return false
-  elsif won?(board)
     return false
   end
 end
